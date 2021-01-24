cb50bf5eff748cd04135b1f2d660bc6a936ed63f
Takahiro Kato <dozo13189@gmail.com>
2021-01-24 22:06:37 +0900

    new build

--- src/sandbox/enum/sandbox.js
+++ src/sandbox/enum/sandbox.js
@@ -1,21 +1,50 @@
-const sampleArray = [
-  'a',
-  'b',
-  'c'
-]
+// const sampleArray = [
+//   'a',
+//   'b',
+//   'c'
+// ]
 
-const sampleObj = Object.assign(
-  ...Object.entries(sampleArray).map(value => ({ [value[1]] : value[0] })),
-  ...Object.entries(sampleArray).map(value => ({ [value[0]] : value[1] }))
-)
+// const sampleObj = Object.assign(
+//   ...Object.entries(sampleArray).map(value => ({ [value[1]] : value[0] })),
+//   ...Object.entries(sampleArray).map(value => ({ [value[0]] : value[1] }))
+// )
 
-console.log(sampleObj)
-console.log(sampleObj.a)
-console.log(sampleObj[0])
+// console.log(sampleObj)
+// console.log(sampleObj.a)
+// console.log(sampleObj[0])
 
+// var People;
+// (function (People) {
+//     People[People["a"] = 0] = "a";
+//     People[People["b"] = 1] = "b";
+//     People[People["c"] = 2] = "c";
+// })(People = {});
+
+
+"use strict";
 var People;
 (function (People) {
     People[People["a"] = 0] = "a";
     People[People["b"] = 1] = "b";
     People[People["c"] = 2] = "c";
-})(People = {});
+})(People || (People = {}));
+(function (People) {
+    People.hoge = 10;
+    function huga() {
+        return "huga";
+    }
+    People.huga = huga;
+})(People || (People = {}));
+
+
+"use strict";
+var People;
+People[People["a"] = 0] = "a";
+People[People["b"] = 1] = "b";
+People[People["c"] = 2] = "c";
+People.hoge = 10;
+People.huga = function huga() {
+    return "huga";
+}
+
+console.log(People)
